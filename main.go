9df684537cfe2e2362600c7d41ea513dc0868094
murtazokhongofurov <gofurovmurtazoxon@gmail.com>
2023-08-09 19:36:16 +0500

    added get list

--- main.go
+++ main.go
@@ -38,7 +38,9 @@ func main() {
 	//}
 	//res, err := Create(db, &user)
 	//CheckError("error Create: ", err)
-	res, err := Get(db, 4)
+	//res, err := Get(db, 4)
+	//_ = Delete(db, 1)
+	res, err := GetList(db)
 	CheckError("error get: ", err)
 	fmt.Println("Data inserted successfully.", res)
 }
@@ -60,19 +62,18 @@ RETURNING
 }
 
 func Update(db *sql.DB, id int) error {
-	var res = User{Name: "Otabek", LastName: "Toshmukhammedov", Age: 25, Phone: "1234567" }
-	query:= "UPDATE users SET first_name=?, last_name=?, age=?, phone=? WHERE id=?"
-	_, err := db.Exec(query, res.Name, res.LastName, res.Age, res.Phone, 2  )
+	var res = User{Name: "Otabek", LastName: "Toshmukhammedov", Age: 25, Phone: "1234567"}
+	query := "UPDATE users SET first_name=?, last_name=?, age=?, phone=? WHERE id=?"
+	_, err := db.Exec(query, res.Name, res.LastName, res.Age, res.Phone, id)
 	CheckError("Update Error:", err)
 	return err
-} 
-
-func Delete(db *sql.DB, Id int) error  {
+}
+func Delete(db *sql.DB, Id int) error {
 	query := "DELETE FROM users WHERE id=?"
 	_, err := db.Exec(query, Id)
 	CheckError("Delete Error", err)
 	return nil
-	
+
 }
 
 func Get(db *sql.DB, id int) (*User, error) {
@@ -84,4 +85,25 @@ SELECT id, first_name, last_name, age, phone FROM users WHERE id=?`
 		return &User{}, err
 	}
 	return &res, nil
+}
+
+func GetList(db *sql.DB) ([]User, error) {
+	res := []User{}
+	query := `
+SELECT 
+    id, first_name, COALESCE(last_name, ''), age, COALESCE(phone, '') 
+FROM users`
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		temp := User{}
+		err = rows.Scan(&temp.Id, &temp.Name, &temp.LastName, &temp.Age, &temp.Phone)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, temp)
+	}
+	return res, nil
 }
